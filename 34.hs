7fc04078108b140cf4f27df15c02c4f14a5377ba
shunsuke wada <letusfly95@gmail.com>
2012-07-22 19:53:26 +0900

    34問目 Calculate Euler's totient

---
+++ 34.hs
@@ -0,0 +1,15 @@
+import Data.Ord (compare)
+
+myGCD :: Int -> Int -> Int
+myGCD l r = case (l `compare` r) of
+	GT -> myGCD (l-r) r
+	LT -> myGCD l     (r-l)
+	EQ -> l
+
+coprime :: Int -> Int -> Bool
+coprime l r 
+    | myGCD l r == 1 = True
+    | otherwise      = False
+
+totient :: Int -> Int
+totient n = length $ filter (\n' -> coprime n' n) [1..n]
