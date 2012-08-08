b77d1c21de2edd8566f74571b89669afee4b3a17
shunsuke wada <letusfly95@gmail.com>
2012-08-09 01:27:38 +0900

    26問目修正

--- 26.hs
+++ 26.hs
@@ -1,21 +1,25 @@
-isListEq :: (Eq a) => [a] -> [a] -> Bool
+import Data.List (sortBy)
+
+isListEq :: (Eq a, Ord a) => [a] -> [a] -> Bool
 isListEq x y = if (x `isListEq'` y) && (y `isListEq'` x) then True
                else False
 
-isListEq' :: (Eq a) => [a] -> [a] -> Bool
+isListEq' :: (Eq a, Ord a) => [a] -> [a] -> Bool
 isListEq' []  _    = True
 isListEq' _   []   = False
 isListEq' (x:xs) y = if (x `elem` y) then isListEq' xs y
                      else False
 
-nubList :: (Eq a) => [[a]] -> [[a]]
-nubList [] = []
-nubList (x:xs) = if any (isListEq x) xs then nubList xs
-                 else x : nubList xs
+nubList :: (Eq a, Ord a) => [[a]] -> [[a]]
+nubList list = nubList' (map (sortBy comp) list)
+    where comp x y = x `compare` y
+nubList' [] = []
+nubList' (x:xs) = if any (isListEq x) xs then nubList xs
+                  else x : nubList xs
 
 combination :: [Int] -> Int -> [[Int]]
 combination n 0 = [n]
-combination n k = map (take k) $ concat [map ((n !! i):) (nubList $ combination (removeAt (i + 1) n) (k - 1)) | i <- [0..length n -1]]
+combination n k = nubList $ map (take k) $ concat [map ((n !! i):) (nubList $ combination (removeAt (i + 1) n) (k - 1)) | i <- [0..length n -1]]
 
 removeAt :: Int -> [Int] -> [Int]
 removeAt _    []   = []
