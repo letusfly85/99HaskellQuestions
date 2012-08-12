dfdb4dc14c1291e4fa75b0fc58a47bec48a45a00
shunsuke wada <letusfly95@gmail.com>
2012-08-12 16:36:54 +0900

    60.hs　答え合わせ

--- 59.hs
+++ 59.hs
@@ -15,3 +15,18 @@ allPattern :: Int -> ([[Int]],(Int,Int))
 allPattern n = let (s,l) = getRange n
                in  if snd s == n then ([[1]],s)
                    else (recCombination ([[fst l..snd l] !! i | i <- [0,1..(fst l)-1]]) , s)
+
+
+{-
+ - the answer on the below site
+ - http://www.haskell.org/haskellwiki/99_questions/Solutions/59
+ -}
+
+type Height = Int
+
+hBalTree :: Height -> a -> [Tree a]
+hBalTree 0 node = [Empty]
+hBalTree 1 node = [Branch node Empty Empty]
+hBalTree h node = [Branch node l r |
+    (hl, hr) <- [(h-1, h-1),(h-2, h-1),(h-1, h-2)]
+    , l <- hBalTree hl node, r <- hBalTree hr node]
--- 60.hs
+++ 60.hs
@@ -1,9 +1,10 @@
 import MyTree
 import MyCombination
+import Maybe (fromJust)
+import Data.List
 
-type Height = Int
 
-maxNode :: Height -> Int
+maxNode :: Int -> Int
 maxNode h = 2^h - 1
 
 recCombination :: [Int] -> [[Int]]
@@ -22,3 +23,34 @@ allPattern n = let (s,l) = getRange n
                    else (recCombination ([[fst l..snd l] !! i | i <- [0,1..(fst l)-1]]) , s)
 
 ans n = length $ hbalTree n
+
+numOfNodes :: Tree a -> Int
+numOfNodes Empty = 0
+numOfNodes (Branch node Empty Empty) = 1
+numOfNodes tree = let (Branch node tl tr) = tree
+                  in  1 + numOfNodes tr + numOfNodes tl
+
+hBalTree :: a -> Int -> [Tree a]
+hBalTree node 0 = [Empty]
+hBalTree node 1 = [Branch node Empty Empty]
+hBalTree node h = [Branch node l r |
+    (hl, hr) <- [(h-1, h-1),(h-2, h-1),(h-1, h-2)]
+    , l <- hBalTree node hl, r <- hBalTree node hr]
+
+minNode :: Int -> Int
+minNode h = minimum $ map numOfNodes $ hBalTree 'x' h
+
+minNodeSeq = 0:1:zipWith ((+).(+1)) minNodeSeq (tail minNodeSeq)
+minNodes = (minNodeSeq !!)
+
+
+balTreeHeight :: Int -> [Int]
+balTreeHeight 1 = [1]
+balTreeHeight 2 = [2,2]
+balTreeHeight n = map (+1) $ balTreeHeight (n-1) ++ balTreeHeight (n-2)
+
+hbalTreeNode _ 0 = [Empty]
+hbalTreeNode x n = concatMap toFilteredTrees [minHeight..maxHeight]
+    where toFilteredTrees = filter ((n ==) . numOfNodes) . hBalTree x
+          minHeight = ceiling $ logBase 2 $ fromIntegral (n+1)
+          maxHeight = (fromJust $ findIndex (>n) minNodeSeq) -1
